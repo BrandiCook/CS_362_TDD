78d1b57c2f49288bf2d628ae9c2dbcd1089e809e
BrandiCook <71798408+BrandiCook@users.noreply.github.com>
2022-02-14 10:34:10 -0600

    Initial commit, no changes
    no comment

---
+++ tests.py
@@ -0,0 +1,22 @@
+# Brandi Cook
+# 93309286
+# TDD Hands On, A2, Due 2/14/2022
+
+import unittest
+from unittest import TestCase
+from check_pwd import check_pwd
+
+# Class definition
+
+# Must be between 8 and 20 characters (inclusive)
+# Must contain at least one lowercase letter
+# Must contain at least one uppercase letter
+# Must contain at least one digit
+# Must contain at least one symbol from: ~`!@#$%^&*()_+-= (copy and paste to avoid missing characters) These are the only permitted symbols
+
+class Test_Password_Validator(TestCase):
+    pass
+
+
+if __name__ == '__main__':
+        unittest.main()
