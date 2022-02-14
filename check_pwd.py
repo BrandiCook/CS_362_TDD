e21788b86cabfad1d8eed894bd1f6ed5e6aad9ee
BrandiCook <71798408+BrandiCook@users.noreply.github.com>
2022-02-14 11:29:46 -0600

    more changes to check_pwd for check_upper and check_lower

--- check_pwd.py
+++ check_pwd.py
@@ -1,16 +1,25 @@
 def check_pwd(password):
     value = True
+    value2 = True
 
     if len(password) > 8 or len(password) > 20:
         return False
 
     for j in password:
         value = False
+        value2 = False
+
         if j.islower():
             value = True
+
         if j.isupper():
-            value = True
-        return value
+            value2 = True
+
+        if value and value2:
+            return True
+        else:
+            return False
+    
     if not value:
         return False
 
