6b36ce9225c54cef0c1c47a81e8ff930a8df37c5
BrandiCook <71798408+BrandiCook@users.noreply.github.com>
2022-02-14 11:41:34 -0600

    Changed logic of check_pwd, previous idea wasn't working

--- check_pwd.py
+++ check_pwd.py
@@ -1,24 +1,18 @@
 def check_pwd(password):
+    lowercase_check = 0
+    uppercase_check = 0
 
     if len(password) > 8 or len(password) > 20:
         return False
 
     for j in password:
-        value = False
-        value2 = False
-
         if j.islower():
-            value = True
+            lowercase_check = 1
+        elif j.isupper():
+            uppercase_check = 1
 
-        if j.isupper():
-            value2 = True
-
-        if not value and not value2:
-            return False
-
-        else:
-            return False
-
-    else:
+    if lowercase_check == 0 or uppercase_check == 0:
         return False
 
+    else:
+        return True
